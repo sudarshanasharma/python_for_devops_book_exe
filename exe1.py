cf7067fc238cc3219d2102a65a8abdd46ae6c5b9
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2021-01-05 14:45:55 -0800

    Update exe1.py

--- exe1.py
+++ exe1.py
@@ -3,3 +3,11 @@ def print_this_name(name):
   print("Name:",name)
 
 print_this_name("Radha")
+
+
+###Write a python function that takes a string as argument and prints whether it is uppercase or lowercase
+def upper_or_lower(word):
+  if word.isupper():
+    print ("It is upper case")
+  else:
+    print ("It is lower case")
