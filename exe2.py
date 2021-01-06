c51bc0539b43fa085bf8548dff3673c2d8fd815b
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2021-01-06 14:44:46 -0800

    Create exe2.py
    initial draft

---
+++ exe2.py
@@ -0,0 +1,14 @@
+###Create a CLI with click that takes a name as argument, and prints it if it starts with p
+#!/usr/bin/env python
+
+import click
+import re
+@click.command()
+@click.option('--name',default='ramsam',help='what izs your name?')
+def print_p_name(name):
+  if re.match(r'(^[Pp]\w+)',name)
+    print(f"{name}")
+  
+  
+if __name__=='__main__':
+  print_p_name(name)
